908db3796fb506c4f0dd1c627235f25c7afae9d3
david <34092600+davidnhn@users.noreply.github.com>
2024-09-18 13:21:44 +0200

    Update fizzbuzz_rainddrop.java

--- fizzbuzz_rainddrop.java
+++ fizzbuzz_rainddrop.java
@@ -21,3 +21,15 @@ class RaindropConverter {
     }
 
 }
+
+class RaindropConverter {
+    String convert(int number) {
+        String result = "";
+
+        if (number % 3 == 0) result += "Pling";
+        if (number % 5 == 0) result += "Plang";
+        if (number % 7 == 0) result += "Plong";
+
+        return result.isEmpty() ? String.valueOf(number) : result;
+    }
+}
