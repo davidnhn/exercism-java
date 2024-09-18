75f84c326f88318c76d01273fc6ac628d00fd67a
david <34092600+davidnhn@users.noreply.github.com>
2024-09-18 13:12:40 +0200

    Create fizzbuzz_rainddrop.java

---
+++ fizzbuzz_rainddrop.java
@@ -0,0 +1,23 @@
+class RaindropConverter {
+  
+    String convert(int number) {
+         StringBuilder sound = new StringBuilder();
+
+        if (number % 3 == 0) {
+            sound.append("Pling");
+        }
+        if (number % 5 == 0) {
+            sound.append("Plang");
+        }
+        if (number % 7 == 0) {
+            sound.append("Plong");
+        }
+
+        if (sound.length() == 0) {
+            return String.valueOf(number);
+        }
+
+        return sound.toString();
+    }
+
+}
